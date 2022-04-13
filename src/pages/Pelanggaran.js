f679c1ce90e882c3d9264b344f5b0cb96d54108f
Noval AKbar Ramadhany <novalakbarramadhany@gmail.com>
2022-04-13 10:00:44 +0700

    New Commit

--- src/pages/Pelanggaran.js
+++ src/pages/Pelanggaran.js
@@ -4,104 +4,141 @@ import { Toast } from "bootstrap";
 import { Modal } from "bootstrap";
 
 export default function Pelanggaran() {
-  let [pelanggaran, dataPelanggaran] = useState([]);
-  let [message, setMessage] = useState("");
-  let [modal, setModal] = useState(null)
-  let [id, setId] = useState(0)
-  let [nama, setNama] = useState("")
+  let [pelanggaran, setPelanggaran] = useState([]);
+  let [message, setMessage] = useState("")
+
+  let [idPelanggaran, setIdPelanggaran] = useState(0)
+  let [namaPelanggaran, setNamaPelanggaran] = useState("")
   let [poin, setPoin] = useState(0)
   let [action, setAction] = useState("")
 
-  //get the token from localstorage
+  let [modal, setModal] = useState(null)
+
+  /** get token from local storage */
   let token = localStorage.getItem(`token-pelanggaran`);
-  //
+
   let authorization = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
-  // create a function to show toast
-  let show = (message) => {
-    let myToast = new Toast(document.getElementById("myToast"), {
-      autohide: true,
-    });
-    // perintah utk mengisi state 'message'
-    setMessage(message);
 
-    // show the toast
-    myToast.show();
-  };
+  /** create function to show Toast */
+  let showToast = message => {
+    let myToast = new Toast(
+      document.getElementById(`myToast`),
+        {
+          autohide: true
+        }
+      )
+      /** perintah utk mengisi state 'message */
+      setMessage(message)
 
-  // create function to get data pelanggaran from backend
+      /** show Toast */
+      myToast.show()
+  }
 
+  /** create function to get data pelanggaran from backend */
   let getData = () => {
-    // akses ke backend untuk proses menampilkan data
-    // method : GET
-    // endpoint : http://localhost:8000/pelanggaran
-    // request : none
-    // response : array data pelanggaran
-    // authorization : bearer token
+    /**
+     * endpoint = http://localhost:8080/pelanggaran
+     * method = GET
+     * request = none
+     * response = array data pelanggaran
+     * authorization = Bearer Token
+     */
+    let endpoint = `http://localhost:8000/pelanggaran`;
 
-    // define endpoint
-    let endpoint = "http://localhost:8000/pelanggaran";
-
-    // sending data
+    /** sending data */
     axios
       .get(endpoint, authorization)
       .then((response) => {
         // simpan di state pelanggaran
-        dataPelanggaran(response.data.Pelanggaran);
+        setPelanggaran(response.data.Pelanggaran);
 
-        //call showToast
-        show(`Data Pelanggaran berhasil dimuat`);
+        /** call showToast */
+        // showToast(`Data pelanggaran berhasil dimuat`)
+
       })
       .catch((error) => console.log(error));
   };
 
   let tambahData = () => {
+    // display modal
     modal.show()
-    // mengosongkan inputan form
-    setId(0)
-    setNama("")
+
+    // mengosongkan inputan form nya
+    setIdPelanggaran(0)
+    setNamaPelanggaran("")
     setPoin(0)
     setAction('insert')
   }
 
-  let editData = (item) => {
+  let editData = item => {
+    // display modal
     modal.show()
 
-    setId(item.id)
-    setNama(item.nama_pelanggaran)
+    // isi form sesuai data yg dipilih
+    setIdPelanggaran(item.id_pelanggaran)
+    setNamaPelanggaran(item.nama_pelanggaran)
     setPoin(item.poin)
     setAction(`edit`)
   }
 
   let simpanData = event => {
     event.preventDefault()
-    //close moda
+    // close modal
     modal.hide()
-    if(action === `insert`){
+    if (action === "insert") {
       let endpoint = `http://localhost:8000/pelanggaran`
       let request = {
-        nama_pelanggaran: nama,
+        nama_pelanggaran: namaPelanggaran,
         poin: poin
       }
 
-      //sending
+      // send data
       axios.post(endpoint, request, authorization)
       .then(response => {
-        show(response.data.message)
-        //refreshdata
+        showToast(response.data.message)
+        // refresh data pelanggaran
         getData()
       })
       .catch(error => console.log(error))
-    }else if(action === `edit`){
+    } else if (action === "edit") {
+        let endpoint = `http://localhost:8000/pelanggaran/${idPelanggaran}`
+        let request = {
+          nama_pelanggaran: namaPelanggaran,
+          poin: poin
+        }
 
+        // sending data utk update pelanggaran
+        axios.put(endpoint, request, authorization)
+        .then(response => {
+          showToast(response.data.message)
+          // refresh data pelanggaran
+          getData()
+        })
+        .catch(error => console.log(error))
+    }
+  }
+
+  let hapusData = item => {
+    if(window.confirm(`Are you sure want to delete this data?`)){
+      let endpoint = `http://localhost:8000/pelanggaran/${item.id_pelanggaran}`
+
+      //sending data
+      axios.delete(endpoint, authorization)
+      .then(response => {
+        showToast(response.data.message)
+        getData()
+      })
+      .catch(error => console.log(error))
     }
   }
 
   useEffect(() => {
-    let modal = new Modal(document.getElementById(`modalPelanggaran`)
+    let modal = new Modal(
+      document.getElementById("modalPelanggaran")
     )
     setModal(modal)
     getData();
@@ -146,7 +183,8 @@ export default function Pelanggaran() {
                     onClick={() => editData(item)}>
                       <span className="fa fa-edit"></span>
                     </button>
-                    <button className="btn btn-sm btn-danger mx-2">
+                    <button className="btn btn-sm btn-danger mx-2"
+                    onClick={() => hapusData(item)}>
                       <span className="fa fa-trash"></span>
                     </button>
                   </div>
@@ -172,8 +210,8 @@ export default function Pelanggaran() {
                   <form onSubmit={ev => simpanData(ev)}>
                     Jenis Pelanggaran
                     <input type="text" className="form-control mb-2" required
-                    onChange={e => setNama(e.target.value)}
-                    value={nama}/>
+                    onChange={e => setNamaPelanggaran(e.target.value)}
+                    value={namaPelanggaran}/>
 
                     Poin Pelanggaran
                     <input type="number" className="form-control mb-2" required
